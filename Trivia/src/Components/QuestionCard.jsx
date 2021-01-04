134f183e37e8e5dc1e003a4db416020d7d05a0db
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-01-04 13:04:22 -0300

    Trivia Project

---
+++ Trivia/src/Components/QuestionCard.jsx
@@ -0,0 +1,233 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { saveScoreToState } from '../actions';
+
+class QuestionCard extends Component {
+  constructor(props) {
+    super();
+
+    const { questions } = props;
+    const answersArray = [...questions.incorrect_answers, questions.correct_answer];
+    answersArray.sort(() => 0.5 - Math.random());
+
+    this.renderButtons = this.renderButtons.bind(this);
+    this.saveScore = this.saveScore.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.setCountdown = this.setCountdown.bind(this);
+    this.checkAnswer = this.checkAnswer.bind(this);
+    this.resetCountdown = this.resetCountdown.bind(this);
+
+    this.state = {
+      answersArray,
+      timerInterval: {},
+      countdown: 30,
+      disabled: false,
+      isCorrect: false,
+      assertions: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.saveToStorage();
+    this.startTimer();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { answerIndex } = this.props;
+    if (prevProps.answerIndex !== answerIndex) {
+      this.updateAnswerIndex();
+      this.resetCountdown();
+    }
+  }
+
+  setCountdown() {
+    const { countdown, timerInterval, isCorrect } = this.state;
+    if (isCorrect) {
+      clearInterval(timerInterval);
+    }
+
+    if (countdown > 0) {
+      return this.setState((previous) => ({
+        ...previous,
+        countdown: previous.countdown - 1,
+      }));
+    }
+
+    clearInterval(timerInterval);
+    this.setState({
+      countdown: 0,
+    });
+    const target = document.querySelectorAll('#wrong-answer')[0];
+    this.checkAnswer({ target });
+  }
+
+  startTimer() {
+    const timerInterval = setInterval(this.setCountdown, 1000);
+    this.storeTimerInSate(timerInterval);
+  }
+
+  resetCountdown() {
+    this.setState({
+      countdown: 30,
+      isCorrect: false,
+    }, () => this.startTimer());
+  }
+
+  saveToStorage() {
+    const { getLogin: { name, gravatarEmail } } = this.props;
+    const { assertions } = this.state;
+    if (!localStorage.state) {
+      const player = {
+        name,
+        score: 0,
+        assertions,
+        gravatarEmail,
+      };
+      localStorage.setItem('state', JSON.stringify({ player }));
+    }
+  }
+
+  changeDisabled(disabled) {
+    this.setState({ disabled });
+  }
+
+  storeTimerInSate(timerInterval) {
+    this.setState({ timerInterval });
+  }
+
+  updateAnswerIndex() {
+    const { questions } = this.props;
+    const tempArray = [...questions.incorrect_answers, questions.correct_answer];
+    tempArray.sort(() => 0.5 - Math.random());
+
+    this.setState({
+      answersArray: tempArray,
+      disabled: false,
+    });
+  }
+
+  decode(text) {
+    const element = document.createElement('textarea');
+    element.innerHTML = text;
+    return element.value;
+  }
+
+  checkAnswer({ target }) {
+    const button = document.querySelectorAll('.btn');
+    Object.values(button).forEach((item) => {
+      if (item.id === 'correct-answer') {
+        item.classList.add('correct');
+      }
+      if (item.id === 'wrong-answer') {
+        item.classList.add('incorrect');
+      }
+    });
+
+    const nextButton = document.querySelector('.invisible');
+    if (nextButton) {
+      nextButton.classList.replace('invisible', 'btn');
+    }
+
+    if (target.id === 'correct-answer') {
+      return this.setState((previous) => ({
+        isCorrect: true,
+        assertions: previous.assertions + 1,
+      }), () => this.saveScore());
+    }
+
+    this.setState({ isCorrect: true });
+    this.changeDisabled(true);
+  }
+
+  saveScore() {
+    const { countdown, assertions } = this.state;
+    const { questions, setScore, getLogin: { name, gravatarEmail } } = this.props;
+    const { difficulty } = questions;
+    const difficultyPoints = { hard: 3, medium: 2, easy: 1 };
+    const points = 10 + (difficultyPoints[difficulty] * countdown);
+
+    if (localStorage.state) {
+      const { player: { score: prevScore } } = JSON.parse(localStorage.getItem('state'));
+      const newScore = prevScore + points;
+      const player = {
+        name,
+        assertions,
+        score: newScore,
+        gravatarEmail,
+      };
+      localStorage.setItem('state', JSON.stringify({ player }));
+      setScore(player);
+    } else {
+      localStorage.setItem('state', JSON.stringify({
+        player: {
+          name,
+          assertions,
+          score: 0,
+          gravatarEmail,
+        },
+      }));
+    }
+  }
+
+  renderButtons(index, question, className) {
+    const { disabled } = this.state;
+    const text = this.decode(question);
+
+    return (
+      <button
+        id={ className }
+        type="button"
+        key={ index }
+        disabled={ disabled }
+        className="btn"
+        data-testid={
+          className === 'wrong-answer' ? `wrong-answer${index}` : 'correct-answer'
+        }
+        onClick={ this.checkAnswer }
+      >
+        {text}
+      </button>
+    );
+  }
+
+  render() {
+    const { answersArray, countdown } = this.state;
+    const { questions } = this.props;
+    const { category, question, correct_answer: correctAnswer } = questions;
+
+    return (
+      <div className="question-container">
+        <h3 data-testid="question-category">{category}</h3>
+        <h4 data-testid="question-text">{this.decode(question)}</h4>
+        <div className="question-answers">
+          { answersArray.map((answer, index) => {
+            if (answer === correctAnswer) {
+              return this.renderButtons(index, answer, 'correct-answer');
+            }
+            return this.renderButtons(index, answer, 'wrong-answer');
+          }) }
+        </div>
+        <p className="countdown">{ countdown }</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  getAnswers: state.questions.questionsArray,
+  getLogin: state.login,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setScore: (score) => dispatch(saveScoreToState(score)),
+});
+
+QuestionCard.propTypes = {
+  answerIndex: PropTypes.number.isRequired,
+  questions: PropTypes.arrayOf(Object).isRequired,
+  getLogin: PropTypes.arrayOf(Object).isRequired,
+  setScore: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionCard);
